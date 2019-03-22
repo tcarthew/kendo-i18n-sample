a33eb0600be439d62b3336cf02c7a9916eaaf597
Tim <tcarthew@gmail.com>
2019-03-22 08:41:19 +0200

    Add items per page translation

--- src/app/services/kendo-translate-service.ts
+++ src/app/services/kendo-translate-service.ts
@@ -2,7 +2,8 @@ import { MessageService } from '@progress/kendo-angular-l10n';
 import { Injectable } from '@angular/core';
 
 const messages = {
-    'kendo.grid.noRecords': 'Nothing to see here.'
+    'kendo.grid.noRecords': 'Nothing to see here.',
+    'kendo.grid.pagerItemsPerPage': 'Show rows'
 };
 
 @Injectable()
